76a4327622b99fdc15db504c295f0328b9b2fc5f
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2018-10-04 12:52:52 -0400

    updated views

--- src/react_ssr/views.py
+++ src/react_ssr/views.py
@@ -21,29 +21,35 @@ class ReactSSRView(View):
     user_serializer_class = None
     reducers_dir = "./frontend/src/reducers"
     extra_context = None
-    extra_request_headers = None
+    extra_render_headers = None
 
-
-    def get_request_headers(self, request):
-        request_headers = {
+    def get_render_headers(self, request):
+        render_headers = {
             "content_type": "application/json"
         }
-
-        # Add the user agent to the headers
         user_agent = request.META.get("HTTP_USER_AGENT", None)
         if user_agent is not None:
-            request_headers.update({
+            render_headers.update({
                 "user-agent": user_agent
             })
+        extra_render_headers = self.get_extra_render_headers(request)
+        render_headers.update(extra_render_headers)
+        return render_headers
 
-        extra_request_headers = self.extra_request_headers
-        if extra_request_headers is not None:
-            request_headers.update(extra_request_headers)
+    def get_extra_render_headers(self, request):
+        extra_render_headers = self.extra_render_headers
+        if extra_render_headers is None:
+            extra_render_headers = {}
+        return extra_render_headers
 
-        return request_headers
+    def get_extra_context(self, request, *args, **kwargs):
+        extra_context = self.extra_context
+        if extra_context is None:
+            extra_context = {}
+        return extra_context
 
-    def build_context(self, payload):
-        error = payload.get("error", None)
+    def get_context(self, request, response, *args, **kwargs):
+        error = response.get("error", None)
         if error is not None:
             message = error.get("message", None)
             stack = error.get("stack", None)
@@ -56,17 +62,17 @@ class ReactSSRView(View):
                 )
             raise RenderServerError(error)
 
-        html = payload.get("html", None)
+        html = response.get("html", None)
         if html is None:
-            raise RenderServerError("Render server failed to return html. Returned: {}".format(payload))
+            raise RenderServerError("Render server failed to return html. Returned: {}".format(response))
 
-        state = payload.get("state", None)
+        state = response.get("state", None)
         if state is None:
-            raise RenderServerError("Render server failed to return state. Returned: {}".format(payload))
+            raise RenderServerError("Render server failed to return state. Returned: {}".format(response))
 
-        script = payload.get("script", None)
+        script = response.get("script", None)
         if script is None:
-            raise RenderServerError("Render server failed to return loadable script. Returned: {}".format(payload))
+            raise RenderServerError("Render server failed to return loadable script. Returned: {}".format(response))
 
         context = {
             "html": html,
@@ -74,37 +80,32 @@ class ReactSSRView(View):
             "script": script,
         }
 
-        extra_context = self.extra_context
+        extra_context = self.get_extra_context(request, *args, **kwargs)
 
-        if extra_context is not None:
-            context.update(self.extra_context)
+        context.update(extra_context)
 
         return context
 
-    def render(self, request, initial_state):
-        # Convert our initial state into a dictionary to render with react.
-        render_data = {
-            "url": request.path_info,
-            "initialState": initial_state,
-            "secretKey": self.secret_key,
-        }
-
-        request_headers = self.get_request_headers(request)
-
+    def render(self, render_payload, render_headers):
         try:
-            response = requests.post(self.render_url,
-                                     json=render_data,
-                                     headers=request_headers,
-                                     timeout=self.render_timeout)
+            response = requests.post(
+                self.render_url,
+                json=render_payload,
+                headers=render_headers,
+                timeout=self.render_timeout
+            )
         except requests.ReadTimeout:
             raise RenderServerError(
-                "Could not render within time allotted: {}".format(self.render_timeout)
+                "Could not render within time allotted: {}".format(
+                    self.render_timeout
+                )
             )
         except requests.ConnectionError:
             raise RenderServerError(
-                "Could not connect to render server at {}".format(self.render_url)
+                "Could not connect to render server at {}".format(
+                    self.render_url
+                )
             )
-
         if response.status_code != 200:
             raise RenderServerError(
                 "Unexpected response from render server at {} - {}: {}".format(
@@ -113,21 +114,20 @@ class ReactSSRView(View):
                     response.text,
                 )
             )
+        return response.json()
 
-        # build the template context from the response json.
-        context = self.build_context(response.json())
-
-        # return the rendered output using the template and context.
-        return render(request, self.template_name, context)
+    def get_render_payload(self, request, initial_state):
+        return {
+            "url": request.path_info,
+            "initialState": initial_state,
+            "secretKey": self.secret_key,
+        }
 
     def get_default_state(self, reducer_name):
         reducer_dir = os.path.join(self.reducers_dir, reducer_name)
         state_file = os.path.join(reducer_dir, "state.json")
         state = read_json(state_file)
         return state
-
-    def get_default_auth_state(self):
-        return self.get_default_state("auth")
 
     def get_user_serializer_class(self):
         if self.user_serializer_class is None:
@@ -145,39 +145,37 @@ class ReactSSRView(View):
             state = serializer.data
         return state
 
-    def build_auth_state(self, request):
-        auth_state = self.get_default_auth_state()
-        # If the user is authenticated, then add it to the state...
+    def get_auth_state(self, request):
+        auth_state = self.get_default_state("auth")
         if request.user.is_authenticated:
-            # Add the API token and a flag to the initial state
             token = jwt_encode_handler(jwt_payload_handler(request.user))
             auth_state.update({
                 "isAuthenticated": True,
                 "token": token,
             })
-            # Serialize the user into the auth initial state.
             if self.user_serializer is not None:
                 user_state = self.get_user_state(request)
                 auth_state.update({
                     "user": user_state
                 })
-        # return the auth state to use
         return auth_state
 
-    def build_initial_state(self, request, page_state):
-        # Build the final initial state to use for rendering.
-        auth_state = self.build_auth_state(request)
+    def get_initial_state(self, request, page_state):
+        auth_state = self.get_auth_state(request)
         page_state.update({
             "auth": auth_state,
         })
-        # Return the completed initial state
         return page_state
 
-    def build_page_state(self, request, *args, **kwargs):
-        raise Exception("build_page_state() not implemented.")
+    def get_page_state(self, request, *args, **kwargs):
+        raise Exception("get_page_state() not implemented.")
 
     def get(self, request, *args, **kwargs):
-        page_state = self.build_page_state(request, *args, **kwargs)
-        initial_state = self.build_initial_state(request, page_state)
-        return self.render(request, initial_state)
+        page_state = self.get_page_state(request, *args, **kwargs)
+        initial_state = self.get_initial_state(request, page_state)
+        render_payload = self.get_render_payload(request, initial_state)
+        render_headers = self.get_render_headers(request)
+        response = self.render(render_payload, render_headers)
+        context = self.get_context(request, response, *args, **kwargs)
+        return render(request, self.template_name, context)
 
