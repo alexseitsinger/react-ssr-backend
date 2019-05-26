0e1be653675b72416166ec8584ed1c1cea1c7664
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-26 15:53:25 +0000

    test: makes env file more readable

--- src/tests/env.py
+++ src/tests/env.py
@@ -1,8 +1,13 @@
 import sys
 import os
 
+# Setup some variables
 SRC_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT = os.path.dirname(SRC_DIR)
+PACKAGE_NAME = os.path.basename(ROOT).replace("-", "_")
+PACKAGE_DIR = os.path.join(SRC_DIR, PACKAGE_NAME)
 
-sys.path.append(os.path.join(
-    SRC_DIR, os.path.basename(os.path.dirname(SRC_DIR))
-))
+# Add the package directory to the python path so our tests can use it.
+sys.path.append(PACKAGE_DIR)
+
+# Run the tests...
