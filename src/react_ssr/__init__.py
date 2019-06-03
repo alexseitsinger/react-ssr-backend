9d209b6882d74ea3cce036aebddd7b3249ede442
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-03 12:23:54 -0400

    chore: 1.1.0.
    Updates the version from 1.0.0 to 1.1.0.

--- src/react_ssr/__init__.py
+++ src/react_ssr/__init__.py
@@ -1,4 +1,4 @@
 # Do not change this version manually.
 # Versioning is managed by package_controller.
 # To update the version run `pc version --patch | --minor | --major`
-__version__ = "1.0.0"
+__version__ = "1.1.0"
