f2e5479184349ce03599bc216768570e78ade9a5
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-09-18 07:11:27 -0400

    chore: 3.1.0.
    Updates the version from 3.0.0 to 3.1.0.

--- src/react_ssr/__init__.py
+++ src/react_ssr/__init__.py
@@ -1,4 +1,4 @@
 # Do not change this version manually.
 # Versioning is managed by package_controller.
 # To update the version run `pc version --patch | --minor | --major`
-__version__ = "3.0.0"
+__version__ = "3.1.0"
